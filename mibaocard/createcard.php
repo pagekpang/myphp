505c90f51824d8aaf821d25747bf505a72862ffc
pagekpang <pagekpang@qq.com>
2013-02-05 11:56:01 +0800

    mibaocrad create

---
+++ mibaocard/createcard.php
@@ -0,0 +1,11 @@
+<?php
+//import sql
+require '../config.php';
+//import mibao util
+require_once '../includes/dz_mibaocard.php';
+
+//4 size
+define('DZ_MIBAO_PREFIX', '1078');
+
+//cardid:DZ_MIBAO_PREFIX RAND
+
